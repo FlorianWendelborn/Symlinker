09a45232360a1d9fd75e09328640e8ee25f96d47
FlorianWendelborn <flowendgithub@gmail.com>
2013-09-30 21:17:56 +0200

    implemented symbolic link creation

--- symlinker.js
+++ symlinker.js
@@ -1,8 +1,100 @@
+/* Requiring */
 var argv = require('optimist')
-	.usage("Usage: $0 file -s [source] -d [destination]")
+	.usage("Usage: $0 file -s [source] -d [destination] -c [boolean: forceDestinationCreation] -f [boolean: forceSymbolicLinkCreation] -r [boolean: forceSymbolicLinkRecreation")
 	.demand(['_','s','d'])
 	.argv;
 
+var mkdirp = require('mkdirp');
+
 var fs = require('fs');
 
-var file = fs.readFileSync(argv._[0], 'utf8');
+/* Setting Variables */
+// argv
+var filePath = argv._[0];
+var destinationPath = argv.d;
+var sourcePath = argv.s;
+
+var forceCreation = argv.f;
+var forceRecreation = argv.r;
+var forceDestinationCreation = argv.c;
+
+// validation
+var sourceExists;
+var destinationExists;
+
+// resources
+var rawFile;
+var file;
+
+/* Validating */
+try {
+	rawFile = fs.readFileSync(filePath, 'utf8');
+} catch (err) {
+	console.log("Could not read the provided symlinker file.\n" + filePath);
+	process.exit();
+}
+
+sourceExists = fs.existsSync(sourcePath);	
+
+if (!sourceExists) {
+	console.log("Source folder not found.\n" + sourcePath);
+	process.exit();
+}
+
+destinationExists = fs.existsSync(destinationPath);
+
+if (!destinationExists) {
+	console.log("Destination folder not found.\n" + destinationPath);
+	if (!forceDestinationCreation) {
+		console.log("Use -c if you want Symlinker to create the destination folder.");
+		process.exit();
+	} else {
+		console.log("Creating destination folder.");
+		mkdirp.sync(destinationPath);
+	}
+}
+
+/* Parsing Symlinker File */
+
+try {
+	file = JSON.parse(rawFile);
+} catch (err) {
+	console.log("Could not parse the provided symlinker file.\n" + filePath);
+}
+
+/* Iterating */
+for (var i = 0; i < file.length; i++) {
+	// setting path
+	var getPath = sourcePath + "/" + file[i];
+	var putPath = destinationPath + "/" + file[i];
+
+	// checking if source folder is valid
+	var valid = fs.existsSync(getPath);
+
+	if (!valid) {
+		if (!forceCreation) {
+			console.log("Path isn't valid: " + getPath + "\nUse -f if you want Symlinker to force symbolic link creation for invalid sources.");
+			process.exit();
+		}
+	} else {
+		try {
+			var dirs = putPath.slice(0,putPath.lastIndexOf("/"));
+			mkdirp(dirs);
+			var isSymbolicLink = fs.lstatSync(putPath).isSymbolicLink();
+			if (isSymbolicLink) {
+				if (!forceRecreation) {
+					console.log("Symbolic link " + putPath  + " already exists.\nUse -r to force symbolic link recreation.");
+					process.exit();
+				} else {
+					fs.unlinkSync(putPath);
+				}
+			}
+
+			fs.symlinkSync(getPath, putPath, "dir");
+			console.log("Created " + putPath + "\n");
+		} catch (err) {
+			console.log("Error creating symbolic link " + getPath + " -> " + putPath);
+			process.exit();
+		}
+	}
+}
